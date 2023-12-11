89773fde4f6f0d9e2622c3c1d6d1689680e56dbf
Libby <L.Tang-2@student.tudelft.nl>
2023-12-11 14:23:44 +0100

    Update the region to Noord Holland

--- src/methods/apiRequests/apiFunction.ts
+++ src/methods/apiRequests/apiFunction.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const getGtfsTable = async (tableName: string) => {
-    const res = await axios.get(process.env.REACT_APP_GRAPH_SERVICE + '/geojson/' + tableName);
+    const res = await axios.get(process.env.REACT_APP_GRAPH_SERVICE + '/geojson/' + tableName, {
+        params: { region_ids: '9dbe4ff2-a739-3da3-9d9f-153235118899' },
+    });
     return res.data as GeoJSON.FeatureCollection<GeoJSON.Geometry>;
 };
 
